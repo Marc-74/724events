29ec4a6e12c0e298b2b26bf7a344fa3b18044476
Marc-74 <106396628+Marc-74@users.noreply.github.com>
2024-05-27 19:46:14 +0200

    Debug du slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,35 +1,45 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useData } from "../../contexts/DataContext";
 import { getMonth } from "../../helpers/Date";
-
 import "./style.scss";
 
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
-  );
+
+  // Trie les événements par date décroissante
+  const byDateDesc = data?.focus.sort((evtA, evtB) => new Date(evtB.date) - new Date(evtA.date));
+
+  // Fonction pour passer à la carte suivante
   const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0), 
-      5000
-    );
+    setIndex((prevIndex) => (prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0));
   };
+
+  // Défini un minuteur pour passer automatiquement à la carte suivante toutes les 5 secondes
   useEffect(() => {
-    nextCard();
-  });
+    const timer = setTimeout(nextCard, 5000);
+
+    // Nettoye le minuteur pour empêcher qu'il fonctionne en arrière-plan
+    return () => clearTimeout(timer);
+  }, [index, byDateDesc]);
+
+  // Gére les changements des boutons radio pour passer à une carte spécifique
+  const handleRadioChange = (radioIdx) => {
+    setIndex(radioIdx);
+  };
+
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
+    
+        <React.Fragment key={`slider-${idx}`.toString()}>
           <div
-            key={event.title} 
+            key={event.title}
             className={`SlideCard SlideCard--${
               index === idx ? "display" : "hide"
             }`}
           >
-            <img src={event.cover} alt="forum" /> 
+            <img src={event.cover} alt="forum" />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
                 <h3>{event.title}</h3>
@@ -40,17 +50,25 @@ const Slider = () => {
           </div>
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
+         
+              {byDateDesc.map((eventItem, radioIdx) => (
+      
                 <input
-                  key={`${event.id}`}
+           
+                  key={`${eventItem.date}`}
                   type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
+             
+                  name={`radio-button-${radioIdx}`}
+              
+         
+                  checked={index === radioIdx}
+            
+                  onChange={() => handleRadioChange(radioIdx)}
                 />
               ))}
             </div>
           </div>
-        </>
+        </React.Fragment>
       ))}
     </div>
   );
